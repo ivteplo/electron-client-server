cb8363ddbadacd40e8dd497bb807bc432549c221
Ivan Teplov <iteplov7@gmail.com>
2025-04-26 13:04:05 +0200

    Add documentation

---
+++ README.md
@@ -0,0 +1,33 @@
+# Back-End Server Emulation for Electron Applications
+
+When creating an Electron app, you might find yourself having to deal with interprocess communication (IPC). However, you can use the familiar client-server communication approach from web development. **Without starting a local server**. For better understanding, please refer to [the article](https://ivanteplov.vercel.app/en/blog/2025-04-25-electron-client-server-architecture).
+
+## Installation
+
+```bash
+npm install electron-client-server electron
+```
+
+## Usage
+
+First, import the library in your main module (where the Electron app is initialized):
+
+```javascript
+import { applyProxy } from "electron-client-server"
+```
+
+Then declare a constant containing the URL at which your frontend can reach the backend.
+
+```javascript
+const baseURL = new URL("http://app.local/")
+```
+
+After your Electron app finished initializing, apply the proxy:
+
+```javascript
+app.whenReady().then(() => {
+    applyProxy(baseURL, server)
+})
+```
+
+There is also an example project in [the `example` folder](./example/) of the repository.
--- library/index.js
+++ library/index.js
@@ -3,43 +3,4 @@
 // Licensed under the Apache license 2.0
 //
 
-import inject from "light-my-request"
-import { net, protocol } from "electron"
-
-import { emulateRequest } from "./emulate.js"
-
-/**
- * Create a proxy to handle requests to an emulated server.
- * @param {URL} baseURL - URL at which the emulated server is available for the frontend.
- * @param {inject.DispatchFunc} server - server to emulate.
- * @returns {(request: Request) => Promise<Response>} - proxy to pass to the `protocol.handle` method from electron.
- */
-export function createProxy(baseURL, server) {
-	return request => {
-		const url = new URL(request.url)
-
-		// If the host doesn’t match our base URL:
-		if (url.hostname !== baseURL.hostname) {
-			// Simply send the request.
-			return net.fetch(request)
-		}
-
-		// Otherwise we want to emulate the request.
-		return emulateRequest({ server, baseURL, url, request })
-	}
-}
-
-/**
- * Apply a proxy to handle requests to an emulated server.
- * @param {URL} baseURL - URL at which the emulated server is available for the frontend.
- * @param {inject.DispatchFunc} server - server to emulate.
- * @returns {void}
- */
-export function applyProxy(baseURL, server) {
-	// Register our proxy
-	protocol.handle(
-		// We do not want the ‘:’ at the end
-		baseURL.protocol.slice(0, -1),
-		createProxy(baseURL, server)
-	)
-}
+export * from "./proxy.js"
