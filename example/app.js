f9e013c58a5dd0842d730899ae84219fd210474b
Ivan Teplov <iteplov7@gmail.com>
2025-06-07 18:53:25 +0200

    Add support for server-sent events

--- example/app.js
+++ example/app.js
@@ -8,11 +8,22 @@ import { app, BrowserWindow } from "electron"
 import { applyProxy } from "../library/index.js"
 import { server } from "./server.js"
 
+// There are two examples available:
+// • a minimal one (ping)
+// • one using Server-Sent Events (sse)
+const example = process.argv?.[2] ?? "ping"
+
+// This is the URL that our "server" will be available at
 const baseURL = new URL("http://example-app.local/")
 
 function createMainWindow() {
 	const window = new BrowserWindow()
-	const url = new URL("./api/ping", baseURL)
+
+	// Load the right URL based on the argument passed to the script
+	const url = example === "ping"
+		? new URL("./api/ping", baseURL)
+		: new URL("./index.html", baseURL)
+
 	window.loadURL(url.toString())
 }
 
